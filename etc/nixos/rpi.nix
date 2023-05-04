2a8d5937b01892901d92f9d2037a71c65cb1707d
En3rGy <en3rgy@web.de>
2023-05-04 06:59:02 +0200

    updated users

--- etc/nixos/rpi.nix
+++ etc/nixos/rpi.nix
@@ -22,14 +22,16 @@
 
   # File systems configuration for using the installer's partition layout
   fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-label/FIRMWARE";
-      fsType = "vfat";
-    };
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
     };
+  };
+
+  time.timeZone = "Europe/Berlin";
+  services.timesyncd = {
+	enable = true;
+	servers = [ "pool.ntp.org" ];
   };
 
   # Preserve space by sacrificing documentation and history
@@ -54,11 +56,11 @@
   };
 
   # Ensure the user exists on the system
-  users.users."en3rgy" = {
+  users.users."raspi" = {
     isNormalUser = true;
-    home = "/home/en3rgy";
-    description = "En3rGy";
-    extraGroups = ["wheel"]; # Add the user to the 'wheel' group for sudo access, if needed
+  };
+
+  users.users."root" = {
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAg2st6LtwRQVQHimkHYOfftw8U9mXz1dMYigN+VvHOhVdrvPxnywB4bciZKJgVuDbzg6eKXiojOuJje3VJKVa1YCL1OCh+ox0udm43OqQeo8FDJhxXzLVDKSOsxAajFBB8WsHb9zOJE0FXkCMK5Ez4UXdQwM31aYkOqMwUt1+CLKGIj/w3SRqQI97ovIuxMQtUoYtSd9tFIl5SjfO3mH68u7ENaBvHxfBJV62vuJJHx8ZZvRQelHJg1K0inGY1hPQqzV2UV7tbQnQHc64ZStoBNprkHkv6WQgq7dEuEXZOkY6TnNkkdXaKKfwYcO6C0t+s0nl0rytQ1Io9+FPmcAcVQ== en3rgy@localhost"
     ];
