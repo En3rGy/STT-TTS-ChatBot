28264b589d501db1118f36221527abef9eed2010
En3rGy <en3rgy@web.de>
2023-05-07 22:49:59 +0200

    fix ;

--- etc/nixos/users.nix
+++ etc/nixos/users.nix
@@ -20,7 +20,8 @@
           "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAg2st6LtwRQVQHimkHYOfftw8U9mXz1dMYigN+VvHOhVdrvPxnywB4bciZKJgVuDbzg6eKXiojOuJje3VJKVa1YCL1OCh+ox0udm43OqQeo8FDJhxXzLVDKSOsxAajFBB8WsHb9zOJE0FXkCMK5Ez4UXdQwM31aYkOqMwUt1+CLKGIj/w3SRqQI97ovIuxMQtUoYtSd9tFIl5SjfO3mH68u7ENaBvHxfBJV62vuJJHx8ZZvRQelHJg1K0inGY1hPQqzV2UV7tbQnQHc64ZStoBNprkHkv6WQgq7dEuEXZOkY6TnNkkdXaKKfwYcO6C0t+s0nl0rytQ1Io9+FPmcAcVQ== root"
         ];
 
-    };
+      };
+	};
 
     root = {
       openssh.authorizedKeys = {
@@ -32,3 +33,4 @@
     };
   };
 }
+
