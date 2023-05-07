6290d17d38033e4853923207fdcb9edd468955c1
En3rGy <en3rgy@web.de>
2023-05-07 22:00:26 +0200

    added users.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,6 +1,5 @@
 { config, pkgs, lib, ... }:
 let
-  username = "raspi";
   my-python-packages = ps: with ps; [
     pyaudio
 	openai
@@ -24,6 +23,11 @@ let
 in 
 {
   system.stateVersion = "22.11";
+  
+  # import additional nix configurations files
+  imports = [
+    ./users.nix
+  ];
 
   # NixOS wants to enable GRUB by default
   boot.loader.grub.enable = false;
@@ -74,35 +78,17 @@ in
   # audio
   sound.enable = true;
   hardware.pulseaudio.enable = true;
- 
-  services.xserver = {
-    layout = "de";
-    xkbOptions = "grp:win_space_toggle";
-  };
-  
+   
   networking.firewall.allowedTCPPorts = [ 80 443 22 ];
   
   i18n = {
     defaultLocale = "de_DE.UTF-8";
     supportedLocales = [ "de_DE.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
   };  
-  
-  # users
-  services.openssh = {
-    enable = true;
-    permitRootLogin = "yes";	
-    passwordAuthentication = false;
+
+  services.xserver = {
+    layout = "de";
+    # xkbOptions = "grp:win_space_toggle";
   };
 
-  users.users.${username} = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-  };
-
-  users.users."root" = {
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAg2st6LtwRQVQHimkHYOfftw8U9mXz1dMYigN+VvHOhVdrvPxnywB4bciZKJgVuDbzg6eKXiojOuJje3VJKVa1YCL1OCh+ox0udm43OqQeo8FDJhxXzLVDKSOsxAajFBB8WsHb9zOJE0FXkCMK5Ez4UXdQwM31aYkOqMwUt1+CLKGIj/w3SRqQI97ovIuxMQtUoYtSd9tFIl5SjfO3mH68u7ENaBvHxfBJV62vuJJHx8ZZvRQelHJg1K0inGY1hPQqzV2UV7tbQnQHc64ZStoBNprkHkv6WQgq7dEuEXZOkY6TnNkkdXaKKfwYcO6C0t+s0nl0rytQ1Io9+FPmcAcVQ== root"
-    ];
-    keyFiles = [ ./3d197d7caf75ae14eeb45c1dc1fa6356_public_2023-05-07.pub ];
-  };
 }
